e58bcad8d59a118df87cf340ed1eb2fbe3422a15
Pavol Lukča <pavol.lukca@gmail.com>
2023-12-01 22:55:06 +0100

    feat: AppBar + theme + CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,56 @@
-import "./App.css";
+import {
+  AppBar,
+  Box,
+  createTheme,
+  CssBaseline,
+  IconButton,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useMemo } from "react";
 
 function App() {
+  const theme = useTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Activity Time Tracker
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+function useTheme() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  return useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode],
   );
 }
 
