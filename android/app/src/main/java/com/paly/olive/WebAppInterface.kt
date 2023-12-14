cd2a441cef7713a8b08fd8232653e84f53a0a591
Pavol Lukča <pavol.lukca@gmail.com>
2023-12-14 22:25:19 +0100

    fix(android): export

---
+++ android/app/src/main/java/com/paly/olive/WebAppInterface.kt
@@ -0,0 +1,36 @@
+package com.paly.olive
+
+import android.content.ClipData
+import android.content.Context
+import android.content.Intent
+import android.webkit.JavascriptInterface
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+
+class WebAppInterface(private val context: Context) {
+    @JavascriptInterface
+    fun export(json: String, filename: String): String {
+        val file = File(context.filesDir, filename)
+        try {
+            FileWriter(file).use { fileWriter -> fileWriter.write(json) }
+        } catch (e: IOException) {
+            return "error"
+        }
+
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("application/json")
+        val fileUri = FileProvider.getUriForFile(context, "com.paly.olive.fileprovider", file)
+        // fixes: java.lang.SecurityException: Permission Denial: reading androidx.core.content.FileProvider uri content://com.paly.olive.fileprovider/files/activities.json from pid=10935, uid=1000 requires the provider be exported, or grantUriPermission()
+        // https://stackoverflow.com/a/75015592/7946803
+        shareIntent.clipData = ClipData.newRawUri("", fileUri)
+        shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri)
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+        startActivity(context, Intent.createChooser(shareIntent, "Share $filename"), null)
+
+        return "ok"
+    }
+}
--- src/routes/storage/ExportButton.tsx
+++ src/routes/storage/ExportButton.tsx
@@ -1,21 +1,54 @@
-import { Button } from "@mui/material";
+import { Alert, Button, Snackbar } from "@mui/material";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import { exportDB } from "../../data/Storage";
 import { saveAs } from "file-saver";
+import { signal } from "@preact/signals-react";
 
 export const ExportButton = () => {
   return (
-    <Button
-      variant={"outlined"}
-      startIcon={<FileDownloadIcon />}
-      onClick={async () => {
-        const data = await exportDB();
-        const json = JSON.stringify(data, null, 2);
-        const blob = new Blob([json], { type: "text/plain;charset=utf-8" });
-        saveAs(blob, "activities.json");
-      }}
-    >
-      Export
-    </Button>
+    <>
+      <Button
+        variant={"outlined"}
+        startIcon={<FileDownloadIcon />}
+        onClick={async () => {
+          // TODO warning about in-progress activities
+          const data = await exportDB();
+          const json = JSON.stringify(data, null, 2);
+          const filename = "activities.json";
+          if (window.Android) {
+            const result = window.Android.export(json, filename);
+            if (result === "error") {
+              androidError.value = true;
+            }
+          } else {
+            const blob = new Blob([json], {
+              type: "application/json;charset=utf-8",
+            });
+            saveAs(blob, filename);
+          }
+        }}
+      >
+        Export
+      </Button>
+      {window.Android && <AndroidError />}
+    </>
   );
 };
+
+const androidError = signal(false);
+
+const AndroidError = () => (
+  <Snackbar
+    open={androidError.value}
+    autoHideDuration={6000}
+    onClose={() => (androidError.value = false)}
+  >
+    <Alert
+      onClose={() => (androidError.value = false)}
+      severity={"error"}
+      sx={{ width: "100%" }}
+    >
+      Failed to export
+    </Alert>
+  </Snackbar>
+);
