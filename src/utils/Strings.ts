f2bf3543f761482ad2589f5202db3f4ba6f7b266
Pavol Lukča <pavol.lukca@gmail.com>
2024-01-13 16:54:46 +0100

    fix: improve activity avatar initials

--- src/utils/Strings.ts
+++ src/utils/Strings.ts
@@ -4,8 +4,28 @@ export const useInitials = (name: Signal<string>) =>
   useComputed(() =>
     name.value
       .split(" ")
-      .map((word) => word[0])
+      .filter(createLongWordsFilter())
       .slice(0, 2)
+      .map((word) => word[0])
       .join("")
       .toLocaleUpperCase(),
   );
+
+const createLongWordsFilter = () => {
+  let removalsLeft: number | undefined = undefined;
+
+  return (word: string, index: number, array: string[]) => {
+    if (removalsLeft === undefined) {
+      removalsLeft = array.length - 2;
+    }
+    const removed =
+      removalsLeft > 0 && (isPreposition(word) || word.startsWith("&"));
+    if (removed) {
+      removalsLeft--;
+    }
+    return !removed;
+  };
+};
+
+const isPreposition = (word: string) =>
+  ["a", "and", "at", "in", "of", "on", "or", "the"].includes(word);
