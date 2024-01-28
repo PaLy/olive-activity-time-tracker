c9a445d2ede8a2648cdcfdacffdcc155b2eb568c
Pavol Lukča <pavol.lukca@gmail.com>
2024-01-28 19:04:59 +0100

    fix: add activity modal scrolling

--- src/routes/storage/StorageModal.tsx
+++ src/routes/storage/StorageModal.tsx
@@ -17,30 +17,32 @@ export const StorageModal = () => {
 
 const Content = () => {
   return (
-    <>
-      <FullScreenModalHeader headline={"Storage"} />
-      <Grid
-        container
-        sx={{ p: 1, height: "calc(100% - 56px);" }}
-        direction={"column"}
-        justifyContent={"space-between"}
-      >
-        <Box sx={{ ml: 1, mr: 1 }}>
-          <Typography variant={"body1"}>
-            All the data is stored only on your device.
-          </Typography>
-          <Typography variant={"body1"}>
-            It is recommended to regularly back up your data.
-          </Typography>
-          <Grid container justifyContent={"space-evenly"} sx={{ mt: 4 }}>
-            <ExportButton />
-            <ImportButton />
-          </Grid>
-        </Box>
-        <Grid container justifyContent={"center"}>
-          <DeleteDataButton />
+    <Grid
+      container
+      minHeight={"100%"}
+      justifyContent={"space-between"}
+      direction={"column"}
+    >
+      <Box>
+        <FullScreenModalHeader headline={"Storage"} />
+        <Grid container sx={{ p: 1 }} direction={"column"}>
+          <Box sx={{ ml: 1, mr: 1 }}>
+            <Typography variant={"body1"}>
+              All the data is stored only on your device.
+            </Typography>
+            <Typography variant={"body1"}>
+              It is recommended to regularly back up your data.
+            </Typography>
+            <Grid container justifyContent={"space-evenly"} sx={{ mt: 4 }}>
+              <ExportButton />
+              <ImportButton />
+            </Grid>
+          </Box>
         </Grid>
+      </Box>
+      <Grid container justifyContent={"center"}>
+        <DeleteDataButton />
       </Grid>
-    </>
+    </Grid>
   );
 };
