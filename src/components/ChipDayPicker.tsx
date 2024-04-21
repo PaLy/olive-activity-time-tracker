a717f3d2d2289f90d9ccc8874f45d51b307b07ed
Pavol Lukča <pavol.lukca@gmail.com>
2024-04-21 21:28:30 +0200

    feat: use calendar time in activity details

--- src/components/ChipDayPicker.tsx
+++ src/components/ChipDayPicker.tsx
@@ -1,21 +1,8 @@
 import { ChipDatePicker, ChipDatePickerProps } from "./ChipDatePicker";
+import { calendarTime } from "../utils/Date";
 
 type Props = Omit<ChipDatePickerProps, "toLabel">;
 
 export const ChipDayPicker = (props: Props) => {
-  return (
-    <ChipDatePicker
-      {...props}
-      toLabel={(value) =>
-        value?.calendar(null, {
-          lastDay: "[Yesterday]",
-          sameDay: "[Today]",
-          nextDay: "[Tomorrow]",
-          lastWeek: "[last] dddd",
-          nextWeek: "dddd",
-          sameElse: "ddd, MMM D, YYYY",
-        })
-      }
-    />
-  );
+  return <ChipDatePicker {...props} toLabel={calendarTime} />;
 };
