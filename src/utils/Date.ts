3794291b486c2bd49d078c45fcc92f5b3b29a4ac
Pavol Lukča <pavol.lukca@gmail.com>
2024-04-21 21:28:30 +0200

    feat: use calendar time in activity details

--- src/utils/Date.ts
+++ src/utils/Date.ts
@@ -1,1 +1,13 @@
+import { Moment } from "moment";
+
 export const MAX_DATE_MS = 8_640_000_000_000_000;
+
+export const calendarTime = (value: Moment) =>
+  value?.calendar(null, {
+    lastDay: "[Yesterday]",
+    sameDay: "[Today]",
+    nextDay: "[Tomorrow]",
+    lastWeek: "[Last] dddd",
+    nextWeek: "dddd",
+    sameElse: "ddd, MMM D, YYYY",
+  });
