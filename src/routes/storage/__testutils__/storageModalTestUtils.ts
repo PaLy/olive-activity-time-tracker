d45550e79639d46eb67e6ab6204b462c976c68e2
Pavol Lukča <pavol.lukca@gmail.com>
2024-04-21 16:49:29 +0200

    fix: old data shown after import

---
+++ src/routes/storage/__testutils__/storageModalTestUtils.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from "fs";
+import { screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+
+export const storageModal = {
+  userEvent: {
+    importFile: async (filepath: string) => {
+      const fileText = readFile(filepath);
+      const file = new File([new Blob([fileText])], "activities.json", {
+        type: "text/json",
+      });
+      File.prototype.text = jest.fn().mockResolvedValueOnce(fileText);
+
+      const input = await screen
+        .findByRole("button", { name: "Import" })
+        .then(
+          (button) =>
+            button.querySelector('input[type="file"]')! as HTMLInputElement,
+        );
+
+      await userEvent.upload(input, file);
+      expect(
+        await screen.findByText("Data successfully imported"),
+      ).toBeVisible();
+    },
+  },
+};
+
+const readFile = (path: string) => readFileSync(path).toString();
