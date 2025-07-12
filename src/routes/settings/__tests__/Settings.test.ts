316718db54e61fee0bef65c211e99f9c86820c7f
Pavol Lukča <pavol.lukca@gmail.com>
2025-07-12 22:28:11 +0200

    fix(Activities): activity shown data improvements

--- src/routes/settings/__tests__/Settings.test.ts
+++ src/routes/settings/__tests__/Settings.test.ts
@@ -4,9 +4,14 @@ import { act, screen } from "@testing-library/react";
 import { element, renderApp } from "../../../__testutils__/app";
 import { userEvent } from "@testing-library/user-event";
 import { activityItem } from "../../activitylists/__testutils__/activityItemUtils";
+import { setLocale } from "../../../utils/Locale";
 
 describe("Settings", () => {
-  it("can enable displaying cost", async () => {
+  beforeAll(() => {
+    setLocale("en-US");
+  });
+
+  beforeEach(async () => {
     await importActivities([
       {
         id: "1",
@@ -16,13 +21,23 @@ describe("Settings", () => {
         ],
       },
     ]);
-    await act(() => renderApp({ route: "/today/settings" }));
+  });
 
+  it("can enable displaying cost", async () => {
+    await act(() => renderApp({ route: "/today/settings" }));
     await userEvent.click(await element.find.switch(/Show cost/i));
     await userEvent.click(await screen.findByRole("button", { name: /back/i }));
 
     expect(await activityItem.find.durationText()).toBe(
-      "100 % • €5.00 30 minutes",
+      "100 % • €5.0030 minutes",
     );
+  });
+
+  it("can show just duration", async () => {
+    await act(() => renderApp({ route: "/today/settings" }));
+    await userEvent.click(await element.find.switch(/Show percentage/i));
+    await userEvent.click(await screen.findByRole("button", { name: /back/i }));
+
+    expect(await activityItem.find.durationText()).toBe("30 minutes");
   });
 });
