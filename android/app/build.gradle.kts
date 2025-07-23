207b59bb81d0811f0efdff4016d2aeec120938ff
Pavol Lukča <pavol.lukca@gmail.com>
2025-07-24 00:29:53 +0200

    chore: version 7 (1.5)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.paly.olive"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.4"
+        versionCode = 7
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
